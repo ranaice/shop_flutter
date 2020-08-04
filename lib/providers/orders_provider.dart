a26f8de70e01608baae730986f6944a1774a2d98
Rodrigo Anaice <rodrigoanaice@hotmail.com>
2020-08-04 16:52:41 -0300

    save orders

--- lib/providers/orders_provider.dart
+++ lib/providers/orders_provider.dart
@@ -1,11 +1,14 @@
+import 'dart:convert';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:shop_flutter/models/cart_item.dart';
 import 'package:shop_flutter/models/order.dart';
 import 'package:shop_flutter/providers/cart_provider.dart';
+import 'package:http/http.dart' as http;
 
 class OrdersProvider with ChangeNotifier {
+  final String _baseUrl = 'https://shop-coder.firebaseio.com/orders';
   List<Order> _items = [];
 
   List<Order> get items {
@@ -14,14 +17,30 @@ class OrdersProvider with ChangeNotifier {
 
   int get itemCount => _items.length;
 
-  void addOrder(CartProvider cart) {
+  Future addOrder(CartProvider cart) async {
+    final date = DateTime.now();
+    final response = await http.post('$_baseUrl.json',
+        body: json.encode({
+          'total': cart.totalAmount,
+          'date': date.toIso8601String(),
+          'products': cart.items.values
+              .map((cartItem) => {
+                    'id': cartItem.id,
+                    'productId': cartItem.productId,
+                    'title': cartItem.title,
+                    'quantity': cartItem.quantity,
+                    'price': cartItem.price,
+                  })
+              .toList(),
+        }));
+
     _items.insert(
       0,
       Order(
-        id: Random().nextDouble().toString(),
+        id: json.decode(response.body)['name'],
         total: cart.totalAmount,
         products: cart.items.values.toList(),
-        date: DateTime.now(),
+        date: date,
       ),
     );
 
